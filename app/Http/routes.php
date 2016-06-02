3af97199df44abafc446b3a108f5d8558b0282e3
Eduardo Reveles <me@osiux.ws>
2016-06-02 02:16:17 -0500

    Catch-all route for vue-router history mode

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -4,11 +4,18 @@ Route::get('/', [
     'uses' => 'HomeController@index',
 ]);
 
+Route::get('/{vue_capture?}', [
+    'as' => 'home',
+    'uses' => 'HomeController@index',
+])->where('vue_capture', '[\/\w\.-]*');
+
 /**
  * Api Routes
  */
 $api = app('Dingo\Api\Routing\Router');
 
 $api->version('v1', ['namespace' => 'DonaSangre\Http\Controllers\Api'], function ($api) {
-
+    $api->post('register', 'AuthController@register');
+    $api->post('login', 'AuthController@login');
+    $api->post('refresh', 'AuthController@refreshToken');
 });
