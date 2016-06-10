fdd3ca3f87f2e8877f064c0b4a05e999094ee8a8
Eduardo Reveles <me@osiux.ws>
2016-06-09 22:55:03 -0500

    Remove unused route middleware

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -50,7 +50,5 @@ class Kernel extends HttpKernel
         'can' => \Illuminate\Foundation\Http\Middleware\Authorize::class,
         'guest' => \DonaSangre\Http\Middleware\RedirectIfAuthenticated::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
-        'jwt.auth' => Tymon\JWTAuth\Middleware\GetUserFromToken::class,
-        'jwt.refresh' => Tymon\JWTAuth\Middleware\RefreshToken::class,
     ];
 }
